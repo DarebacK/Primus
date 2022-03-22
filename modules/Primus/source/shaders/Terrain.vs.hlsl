0919478e06fd3876ee42e692b937a346313b888e
DarebacK <dareback1@gmail.com>
2022-03-22 20:37:05 +0100

    Sea geometry has now zero elevation.

--- modules/Primus/source/shaders/Terrain.vs.hlsl
+++ modules/Primus/source/shaders/Terrain.vs.hlsl
@@ -27,12 +27,12 @@ Output vertexShaderMain(Input input)
 	const uint x = input.vertexIndex % heightmapWidth;
 	const uint y = input.vertexIndex / heightmapWidth;
 
-	min16int height = heightmap.Load(int3(x, y, 0));
+	const float height = float(heightmap.Load(int3(x, y, 0)));
 
 	Output output;
 	output.uv.x = x / float(heightmapWidth - 1);
 	output.uv.y = y / float(heightmapHeight - 1);
-	const float4 positionWorld = float4(output.uv.x, height, 1.f - output.uv.y, 1.f);
+	const float4 positionWorld = float4(output.uv.x, max(height, 0.f), 1.f - output.uv.y, 1.f);
 	output.position = mul(positionWorld, transform);
 	output.height = float(height);
 	return output;
